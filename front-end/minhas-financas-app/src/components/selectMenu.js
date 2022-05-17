dc1174e249b0e02078bc70f6123213402b037de5
leonardo <demetrio.vd@hotmail.com>
2022-05-17 13:51:06 -0300

    launch menu

---
+++ front-end/minhas-financas-app/src/components/selectMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function SelectMenu (props) {
+    const options = props.lista.map((option, index) => {
+        return ( 
+            <option key={index} value={option.value}>{option.label}</option>
+        )
+    })
+
+
+    return (
+        <select {...props}>
+            {options}
+        </select>
+    )
+}
+
+export default SelectMenu
--- front-end/minhas-financas-app/src/views/consulta-lancamentos.js
+++ front-end/minhas-financas-app/src/views/consulta-lancamentos.js
@@ -2,8 +2,31 @@ import React from "react";
 import {withRouter} from 'react-router-dom'
 import Card from "../components/card";
 import FormGroup from "../components/formgroup";
+import SelectMenu from "../components/selectMenu";
+
 class ConsultaLancamentos extends React.Component{
     render(){
+        const meses = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Janeiro', value: 1},
+            {label: 'Fevereiro', value: 2},
+            {label: 'Março', value: 3},
+            {label: 'Abril', value: 4},
+            {label: 'Maio', value: 5},
+            {label: 'Junho', value: 6},
+            {label: 'Julho', value: 7},
+            {label: 'Agosto', value: 8},
+            {label: 'Setembro', value: 9},
+            {label: 'Outubro', value: 10},
+            {label: 'Novembro', value: 11},
+            {label: 'Dezembro', value: 12},
+        ]
+
+        const tipos = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Despesa', value: 'DESPESA'},
+            {label: 'Receita', value: 'RECEITA'},
+        ]
         return(
             <Card title="Consulta Lancamentos">
                 <div className="row">
@@ -12,7 +35,7 @@ class ConsultaLancamentos extends React.Component{
                             <FormGroup label="Ano" htmlFor="inputAno">
                                     <input 
                                         type="text"
-                                        class="form-control"
+                                        className="form-control"
                                         id="inputAno"
                                         aria-describedby="emailHelp"
                                         placeholder="Digite o Ano"
@@ -20,7 +43,10 @@ class ConsultaLancamentos extends React.Component{
                             </FormGroup>
 
                             <FormGroup label="Mês" htmlFor="inputMes">
-                                    
+                                    <SelectMenu className="form-control" lista={meses}/>
+                            </FormGroup>
+                            <FormGroup label="Tipo" htmlFor="inputTipo">
+                                    <SelectMenu id="inputTipo" className="form-control" lista={tipos}/>
                             </FormGroup>
                         </div>
                     </div>
