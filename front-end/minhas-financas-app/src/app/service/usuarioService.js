2cbfc3476f2be51e78366df1e053d7c7203d55c0
leonardo <demetrio.vd@hotmail.com>
2022-05-23 09:00:43 -0300

    refactoring the register validation

--- front-end/minhas-financas-app/src/app/service/usuarioService.js
+++ front-end/minhas-financas-app/src/app/service/usuarioService.js
@@ -1,5 +1,5 @@
 import ApiService from "../apiService";
-
+import ErroValidacao from '../exception/ErroValidacao' 
 class UsuarioService extends ApiService{
     constructor(){
         super('/api/usuarios')
@@ -16,6 +16,30 @@ class UsuarioService extends ApiService{
 
     salvar(usuario){
         return this.post('/', usuario)
+    }
+
+    validar(usuario){
+        const erros = []
+        if (!usuario.nome){
+            erros.push('O campo nome é obrigatório.')
+        }
+        if (!usuario.email){
+            erros.push('O campo Email é obrigatório')
+        }else if(usuario.email.match(/^[a-z0-9]+@[a-z0-9]+\.[a-z]/)){
+            erros.push('Informe um Email válido')
+        }
+
+        if(!usuario.senha || !usuario.senhaRepeticao){
+            erros.push('Digite a senha 2x')
+        }else if(usuario.senha !== usuario.senhaRepeticao){
+            erros.push('As senhas não batem')
+        }
+
+        if (erros && erros.length > 0){
+            throw new ErroValidacao(erros)
+        }
+
+        return erros;
     }
 }
 
