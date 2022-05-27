e8fbd2affee0f5edeeab56713ee59c4de92a7448
leonardo <demetrio.vd@hotmail.com>
2022-05-27 18:55:56 -0300

    Adding the Readme

---
+++ back-end/README.md
@@ -0,0 +1,22 @@
+# Back-end
+
+This is the project backend. it is running with:
+
+    - Spring boot 
+    - H2 - Test
+    - PostgreSQL 
+
+---
+
+    Unitary tests:
+
+    - Junit5
+
+--- 
+    Dependencies:
+
+    Java JDK 11+
+    Eclipse , Intellij or STS
+
+
+
--- front-end/minhas-financas-app/src/views/cadastrUsuario.js
+++ front-end/minhas-financas-app/src/views/cadastrUsuario.js
@@ -96,6 +96,7 @@ class CadastroUsuario extends React.Component {
                                     name="senha"
                                     onChange={e => this.setState({ senhaRepeticao: e.target.value })}></input>
                             </FormGroup>
+                            <br/>
                             <button onClick={this.cadastrar} type="button" className='btn btn-success'>Salvar</button>
                             <button onClick={this.cancelar} type="button" className='btn btn-danger'>Cancelar</button>
                         </div>
